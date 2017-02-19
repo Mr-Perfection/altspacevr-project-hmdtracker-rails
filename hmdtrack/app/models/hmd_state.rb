3471e1bc6d27cd50aea8d56e6dbc402067cfa48d
Sungsoo lee <sungsoolee0127@gmail.com>
2017-02-19 12:55:13 -0800

    refactored the code into audited_state concern module, worked as expected, and passed all the tests in audited_state test.

--- hmdtrack/app/models/hmd_state.rb
+++ hmdtrack/app/models/hmd_state.rb
@@ -2,13 +2,4 @@ class HmdState < ActiveRecord::Base
   include AuditedState
   # state belongs to hmd and update the updated_at when there exists update.
   is_audited_state_for :hmd
-
-  # validaitons
-
-
-
-  # TODO: Make this work!
-  # include AuditedState
-  #
-  # is_audited_state_for :hmd
 end
