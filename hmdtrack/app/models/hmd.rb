8578a3daa12caf2c259da413449c48caf069de89
Sungsoo lee <sungsoolee0127@gmail.com>
2017-02-18 16:54:24 -0800

    updated new models to handle the requirements. Need to be modulized

--- hmdtrack/app/models/hmd.rb
+++ hmdtrack/app/models/hmd.rb
@@ -1,6 +1,28 @@
 class Hmd < ActiveRecord::Base
+
+  # Hmd has one state from [:announced, :devkit, :released]
+  has_many :hmd_states, dependent: :destroy
+  before_save :save_state
+
+  # create a default hmd_state after create
+  after_create :create_hmd_state
+
+  # def save
+  #   self.hmd_state.save!
+  # end
+  private
+    def create_hmd_state
+      puts "olahhhh! Create"
+      self.hmd_states.build(state: self.state)
+    end
+    def save_state
+      puts "olahhhh! Save"
+      self.hmd_states.build(state: self.state)
+      puts self.hmd_states.all
+    end
   # TODO: Make this work!
   # include AuditedState
   #
   # has_audited_state_through :hmd_states, [:announced, :devkit, :released]
+
 end
--- hmdtrack/app/models/hmd_state.rb
+++ hmdtrack/app/models/hmd_state.rb
@@ -1,6 +1,13 @@
 class HmdState < ActiveRecord::Base
+  # state belongs to hmd and update the updated_at when there exists update.
+  belongs_to :hmd, touch: true
+
+  # validaitons
+
+
+
   # TODO: Make this work!
   # include AuditedState
-  # 
+  #
   # is_audited_state_for :hmd
 end
