df029b8cdd652c2948d6a730cc9969a3ef1d6da9
Sungsoo lee <sungsoolee0127@gmail.com>
2017-02-18 19:18:26 -0800

    added more test cases in hmd_states model

--- hmdtrack/test/models/hmd_state_test.rb
+++ hmdtrack/test/models/hmd_state_test.rb
@@ -3,14 +3,17 @@ require 'test_helper'
 class HmdStateTest < ActiveSupport::TestCase
 
   def setup
-    @hmd = Hmd.new(name: "Rift DK1", company: "Oculus VR", state: "released",
-           announced_at: DateTime.new(2012, 8, 1),
-           image_url: "http://i.imgur.com/EY3KHSz.jpg" )
+    @hmd_state = HmdState.new(hmd_id: 1, state: :announced)
     #@failed_hmd = hmd_infos(:failed_one)
   end
 
   test "new hmd_state should be valid" do
-    assert @hmd.valid?
+    assert @hmd_state.valid?
+  end
+
+  test "new hmd_state should not be valid" do
+    @hmd_state = HmdState.new(hmd_id: 1, state: 'af')
+    assert_not @hmd_state.valid?
   end
 
 
